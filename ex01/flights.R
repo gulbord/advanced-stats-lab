3b623a2b034be98c7f53944eb63153f652049f06
gulbord <guglielmo.bordin@gmail.com>
2023-04-07 17:11:26 +0200

    Add flights.R script with second part's code

---
+++ ex01/flights.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(nycflights13)
+
+font <- "Lato"
+theme_set(theme_minimal(base_size = 14, base_family = font))
+my_palette <- unname(palette.colors(4, "Okabe-Ito"))
+
+flights |>
+    filter(origin %in% c("JFK", "LGA", "EWR")) |>
+    mutate(date = lubridate::make_date(year, month, day)) |>
+    dplyr::count(date, origin) |>
+    ggplot(aes(x = date, y = n, colour = origin)) +
+        geom_point(size = 0.8) +
+        geom_smooth(aes(fill = origin), linewidth = 0.8) +
+        scale_colour_manual(values = my_palette) +
+        scale_fill_manual(values = my_palette, guide = "none") +
+        labs(x = "Date", y = "Number of flights", colour = "Airport") +
+        guides(colour = guide_legend(override.aes = list(fill = NA))
