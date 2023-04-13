1608ea6243485054f13a2d5bf9054b8bb2c5137e
gulbord <52690645+gulbord@users.noreply.github.com>
2023-04-13 11:10:56 +0200

    Copy code for second part to .R script

--- ex01/flights.R
+++ ex01/flights.R
@@ -3,16 +3,77 @@ library(nycflights13)
 
 font <- "Lato"
 theme_set(theme_minimal(base_size = 14, base_family = font))
-my_palette <- unname(palette.colors(4, "Okabe-Ito"))
+my_palette <- rcartocolor::carto_pal(10, "Safe")[1:3]
+
+flights$date <- as_date(flights$time_hour)
 
 flights |>
-    filter(origin %in% c("JFK", "LGA", "EWR")) |>
-    mutate(date = make_date(year, month, day)) |>
     count(date, origin) |>
     ggplot(aes(x = date, y = n, colour = origin)) +
         geom_point(size = 0.8) +
-        geom_smooth(aes(fill = origin), linewidth = 0.8) +
+        geom_smooth(aes(fill = origin), lwd = 0.8) +
         scale_colour_manual(values = my_palette) +
         scale_fill_manual(values = my_palette, guide = "none") +
         labs(x = "Date", y = "Number of flights", colour = "Airport") +
-        guides(colour = guide_legend(override.aes = list(fill = NA))
+        guides(colour = guide_legend(override.aes = list(fill = NA)))
+
+flights |>
+    mutate(day_type = ifelse(wday(date) %in% c(1, 7),
+                             "weekend", "workday")) |>
+    # count by day, keeping info on week and day_type
+    count(week = week(date), day, day_type) |>
+    # regroup by week and day_type to get the averages we want
+    group_by(week, day_type) |>
+    summarize(avg = mean(n)) |>
+    ggplot(aes(x = week, y = avg, colour = day_type)) +
+        geom_line(lwd = 0.8) +
+        scale_colour_manual(values = my_palette,
+                            labels = c("Weekends", "Workdays")) +
+        labs(x = "Week", y = "Average flights per day",
+             colour = "Day type")
+
+by_week <- flights |>
+    dplyr::filter(wday(date) %in% 2:6) |>
+    count(week = week(date))
+
+ind_day <- week("2013-07-04")
+thnxgiv <- week("2013-11-28")
+
+by_week[(ind_day - 2):(ind_day + 2), ] |>
+    kbl(caption = "Flights in the workweeks around July 4th",
+        table.attr = "style='width:50%;'") |>
+    kable_styling(full_width = TRUE, position = "center")
+by_week[(thnxgiv - 2):(thnxgiv + 2), ] |>
+    kbl(caption = "Flights in the workweeks around Thanksgiving",
+        table.attr = "style='width:50%;'") |>
+    kable_styling(full_width = TRUE, position = "center")
+
+dep_delays <- flights |>
+    # some flights don’t have a recorded departure and/or arrival time
+    drop_na(dep_delay) |>
+    group_by(date, origin) |>
+    summarize(avg = mean(dep_delay),
+              min = min(dep_delay),
+              max = max(dep_delay))
+
+plot_delays <- function(key) {
+    names <- c(
+        "Maximum" = "max",
+        "Minimum" = "min",
+        "Average" = "avg"
+    )
+
+    ggplot(dep_delays, aes(x = date, y = .data[[names[key]]],
+                           colour = origin)) +
+        geom_point(size = 0.8) +
+        geom_smooth(aes(fill = origin), lwd = 0.8) +
+        scale_colour_manual(values = my_palette) +
+        scale_fill_manual(values = my_palette, guide = "none") +
+        labs(x = "Date", y = paste(key, "departure delay [min]"),
+            colour = "Airport") +
+        guides(colour = guide_legend(override.aes = list(fill = NA)))
+}
+
+plot_delays("Average")
+plot_delays("Maximum")
+plot_delays("Minimum")
