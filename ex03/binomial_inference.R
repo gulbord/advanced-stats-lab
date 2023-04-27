5af6256845308706f8b7d8c09168244ececcbeea
gulbord <52690645+gulbord@users.noreply.github.com>
2023-04-27 14:12:30 +0200

    Sketch first problem

---
+++ ex03/binomial_inference.R
@@ -0,0 +1,21 @@
+# successes
+y <- 7
+# trials
+n <- 20
+# probability vector
+dp <- 1 / 2000
+p <- seq(0, 1, by = dp)
+
+stepf <- function(x) {
+   ifelse(x <= 0.2, x,
+          ifelse(x <= 0.3, 0.2,
+                 ifelse(x <= 0.5, 0.5 - x, 0)))
+}
+
+post_step_star <- function(x) {
+    stepf(x) * dbinom(y, n, x)
+}
+
+post_step <- post_step_star(p) / integrate(post_step_star, 0, 1)$value
+post_unif <- dbeta(p, 1 + y, 1 + n - y)
+post_jeff <- dbeta(p, 0.5 + y, 0.5 + n - y)
